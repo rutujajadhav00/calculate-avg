2e967cf10841195e9e221844b87e3f91a73c708f
rutujajadhav00 <rutujajadhav413512@gmail.com>
2025-05-08 12:50:24 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 10, 20, 30, 40, 50 };
+        double average = (double)numbers.Sum() / numbers.Length;
+        Console.WriteLine("Average: " + average);
+    }
+}
+
+
