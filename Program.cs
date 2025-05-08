d524bab31ebe4fbe47c04ec4e06a6c60700c6825
rutujajadhav00 <rutujajadhav413512@gmail.com>
2025-05-08 12:53:40 +0530

    number added

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main()
     {
-        int[] numbers = { 10, 20, 30, 40, 50 };
+        int[] numbers = { 20, 20, 30, 40, 50 };
         double average = (double)numbers.Sum() / numbers.Length;
         Console.WriteLine("Average: " + average);
     }
