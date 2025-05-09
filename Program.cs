b5f4cb811aa7e5d9f1ab430ad1adb4d11649f70a
rutujajadhav00 <rutujajadhav413512@gmail.com>
2025-05-09 08:18:24 +0530

    value change

--- Program.cs
+++ Program.cs
@@ -4,9 +4,10 @@ class Program
 {
     static void Main()
     {
-        int[] numbers = { 20, 20, 30, 40, 50 };
-        double average = (double)numbers.Sum() / numbers.Length;
-        Console.WriteLine("Average: " + average);
+        int[] A={ 10,20,30,40};
+        double avg=A.Sum() /A.Length;
+        Console.WriteLine(avg);
+         
     }
 }
 
